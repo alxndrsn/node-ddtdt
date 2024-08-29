227ef14f4737e5ae565ec6fa2589eea39462953b
alxndrsn <alxndrsn>
2024-08-29 13:15:06 +0000

    wip

--- test/index.spec.js
+++ test/index.spec.js
@@ -31,4 +31,38 @@ describe('dataTable', () => {
       });
     });
   });
+
+  describe('two input columns, with values', () => {
+    const fn = (...args) => {
+      return dataTable`
+        ${args.length} | ${args[0]} || type       | colourTxt  | colourEnabled | codeTxt    | codeEnabled
+                     0 |            || Type...    |            | ❌            | Code...    | ✅
+                     1 |            || Type...    |            | ❌            | Code...    | ✅
+                     1 | a-type     || a-type     |            | ❌            | Code...    | ❌
+                     1 | b-type     || other1     |            | ✅            | Code...    | ✅
+                     1 | ${args[0]} || ${args[0]} |            | ✅            | Code...    | ❌
+                     2 | ${args[0]} || ${args[0]} | ${args[1]} | ✅            | Code...    | ✅
+                     3 | ${args[0]} || ${args[0]} | ${args[1]} | ✅            | ${args[2]} | ✅
+      `;
+    };
+
+    it('should reject too many args', () => {
+      // expect
+      assert.throws(() => fn(1, 2, 3, 4), 'No args matched [4]');
+    });
+
+    [
+      [ [],                               ['Type...', '',         false, 'Code...', false] ],
+      [ [''],                             ['Type...', '',         false, 'Code...', false] ],
+      [ ['a-type'],                       ['a-type',  '',         true,  'Code...', false] ],
+      [ ['X-type'],                       ['X-type',  '',         true,  'Code...', false] ],
+      [ ['a-type', 'a-colour'],           ['a-type',  'a-colour', true,  'Code...', true ] ],
+      [ ['a-type', 'a-colour', 'a-code'], ['a-type',  'a-colour', true,  'a-code',  true ] ],
+    ].forEach(([ args, [ type, colourTxt, colourEnabled, codeTxt, codeEnabled ] ]) => {
+      it(`should parse ${args.length} args as expected`, () => {
+        // expect
+        assert.deepEqual(fn(...args), { type, colourTxt, colourEnabled, codeTxt, codeEnabled });
+      });
+    });
+  });
 });
