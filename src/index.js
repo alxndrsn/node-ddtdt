2fa3c5bd9c945dd9d43e838e76d778ec6ba9739f
Alex Anderson <191496+alxndrsn@users.noreply.github.com>
2024-08-30 10:09:25 +0300

    Support multiple input columns (#1)

--- src/index.js
+++ src/index.js
@@ -1,97 +1,67 @@
 module.exports = dataTable;
 
-const ARG_TERM = '||';
+const ARG_DIVIDER = Symbol('||');
+const COL_DIVIDER = Symbol('|');
+
+class Value {
+  constructor(v) {
+    this.v = v;
+  }
+}
 
 function dataTable(parts, ...values) {
-  parts = parts.map(it => it.trim());
-
-  if(parts[0]) throw new Error(`Unexpected text before first arg column: '${parts[0]}'`);
-  parts.shift();
-
-  const args = [ values.shift() ];
-  while(!endOfArgs(parts)) {
-    args.push(values.shift());
-    parts.shift();
+  const full = [ parts[0].trim() ];
+  for(let i=0; i<values.length; ++i) {
+    full.push(new Value(values[i]), parts[i+1]);
   }
+  full[full.length-1] = full[full.length-1].trim();
 
-  if(!parts.length) throw new Error('No remaining parts!');
-  const [ firstLine, ...remaining ] = parts.shift().split('\n');
-  parts.unshift(remaining.join('\n').trim());
-
-  const fieldNames = firstLine.substr(ARG_TERM.length).split('|').map(s => s.trim());
-
-  let state;
-  const rows = [];
-
-  let currentRow;
-  const newCurrent = () => {
-    if(currentRow) rows.push(currentRow);
-    state = 'processing-args';
-    currentRow = { args:[], vals:[] };
-  };
-  newCurrent();
-
-  let currentPart;
-  const popPart = () => {
-    currentPart = parts.shift();
-    if(currentPart === undefined) return false;
-
-    currentPart = currentPart.replace(/^[ \t]*/, '');
-    if(currentPart.startsWith('\n')) {
-      newCurrent();
-      currentPart = currentPart.trim();
+  const lines = [];
+  let currentLine = [];
+  for(const f of full) {
+    if(f instanceof Value) currentLine.push(f);
+    else if(!f.includes('\n')) currentLine.push(f);
+    else {
+      const [endThis, ...others] = f.split('\n');
+      currentLine.push(endThis);
+      lines.push(currentLine);
+      while(others.length > 1) lines.push([ others.shift() ]);
+      currentLine = [ others[0] ];
     }
+  }
+  if(currentLine.length) lines.push(currentLine);
 
-    currentPart = currentPart.trim();
-
-    // There are probably some bugs here relating to empty strings in first
-    // column or first column after ARG_TERM.  TODO write tests and fix.
-    if(currentPart.startsWith(ARG_TERM)) {
-      state = 'processing-vals';
-      currentPart = currentPart.substr(ARG_TERM.length);
-    }
-    if(currentPart.startsWith('|')) {
-      currentPart = currentPart.substr(1).trim();
-    }
-
-    if(!currentPart) {
-      currentPart = values.shift();
-      return true;
-    }
-
-    const unshiftAfter = seq => {
-      if(currentPart.includes(seq)) {
-        const [ a, ...remaining ] = currentPart.split(seq);
-        currentPart = a.trim();
-        parts.unshift(seq + remaining.join(seq));
+  const processedLines = lines.map(line => {
+    const pLine = [];
+    for(const cell of line) {
+      if(cell instanceof Value) pLine.push(cell.v);
+      else {
+        pLine.push(...tokenise(cell));
       }
-    };
-    unshiftAfter(ARG_TERM);
-    unshiftAfter('|');
-    unshiftAfter('\n');
-
-    return currentPart !== undefined;
-  };
-
-  while(popPart()) {
-    switch(state) {
-      case 'processing-args': {
-        currentRow.args.push(parseSpecial(currentPart));
-      } break;
-      case 'processing-vals': {
-        currentRow.vals.push(parseSpecial(currentPart));
-      } break;
-      default: throw new Error(`No handling for state '${state}'`);
     }
-  }
-  newCurrent();
+    const objLine = { args:[], vals:[] };
+    let argsDone = false;
+    for(const cell of pLine) {
+      switch(cell) {
+        case ARG_DIVIDER: argsDone = true; break;
+        case COL_DIVIDER: break;
+        default: objLine[argsDone ? 'vals' : 'args'].push(cell);
+      }
+    }
+    return objLine;
+  });
+
+  const [ { args, vals:fieldNames }, ...rows ] = processedLines;
 
   for(const r of rows) {
     if(args.length !== r.args.length) continue;
 
     // N.B. weak value comparison to allow for easy use of numbers in text.
     // This may cause ambiguities down the line...
-    if(args.some((arg, i) => arg != r.args[i])) continue;
+    if(!args.every((arg, i) => {
+      if(arg == r.args[i]) return true;
+      if(arg == null && r.args[i] === '') return true;
+    })) continue;
 
     return fieldNames.reduce((ret, k, i) => {
       ret[k] = r.vals[i];
@@ -102,13 +72,32 @@ function dataTable(parts, ...values) {
   throw new Error(`No args matched ${JSON.stringify(args)}`);
 }
 
-function endOfArgs([nextPart]) {
-  if(nextPart == null) throw new Error(`String ends before arg terminator ('${ARG_TERM}') seen!`);
-  return nextPart.startsWith(ARG_TERM);
+function tokenise(str) {
+  const replaced = replace(str, '||', ARG_DIVIDER)
+      .map(it => typeof it === 'string' ? replace(it, '|', COL_DIVIDER) : it)
+      .flat();
+  return replaced
+      .filter((v, idx) => {
+        if(typeof v !== 'string') return true;
+        if(v) return true;
+        const prev = replaced[idx-1];
+        const next = replaced[idx+1];
+        if(prev instanceof Value || !prev) return false;
+        if(next instanceof Value || !next) return false;
+        return true;
+      })
+      .map(it => {
+        if(it === '✅') return true;
+        if(it === '❌') return false;
+        return it;
+      });
 }
 
-function parseSpecial(v) {
-  if(v === '✅') return true;
-  if(v === '❌') return false;
-  return v;
+function replace(str, match, replacement) {
+  const splitByArg = str.split(match).map(it => it.trim());
+  const rebuilt = [ splitByArg[0] ];
+  for(let i=1; i<splitByArg.length; ++i) {
+    rebuilt.push(replacement, splitByArg[i]);
+  }
+  return rebuilt;
 }
