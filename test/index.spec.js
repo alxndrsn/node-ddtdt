df40a3e27a055fdc72292b21f14a32d02d946518
alxndrsn <alxndrsn>
2024-08-30 06:52:58 +0000

    add new test

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,6 +3,24 @@ const { assert } = require('chai');
 const dataTable = require('../src');
 
 describe('dataTable', () => {
+  it('should work for hardcoded values', () => {
+    // when
+    const vals = dataTable`
+      m1 | m2 | m3 || a | b | c | d | e | f | g
+         | m2 | m3 || 1 | 2 | 3 | 4 | 5 | 6 | 0
+      m1 |    | m3 || 1 | 2 | 3 | 4 | 5 | 6 | 1
+      m1 | m2 |    || 1 | 2 | 3 | 4 | 5 | 6 | 2
+         |    | m3 || 1 | 2 | 3 | 4 | 5 | 6 | 3
+         | m2 |    || 1 | 2 | 3 | 4 | 5 | 6 | 4
+      m1 |    |    || 1 | 2 | 3 | 4 | 5 | 6 | 5
+         |    |    || 1 | 2 | 3 | 4 | 5 | 6 | 6
+      m1 | m2 | m3 || 1 | 2 | 3 | 4 | 5 | 6 | 7
+    `;
+
+    // then
+    assert.deepEqual(vals, { a:'1', b:'2', c:'3', d:'4', e:'5', f:'6', g:'7' });
+  });
+
   describe('original demo table', () => {
     const fn = (...args) => {
       return dataTable`
